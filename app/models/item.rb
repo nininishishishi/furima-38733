d62895ee5e72cd5c0b61d7e90c8fe6b4b4fcc851
nininishishishi <121347235+nininishishishi@users.noreply.github.com>
2023-04-15 21:45:35 +0900

    レビュー依頼（商品出品機能）#4

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,8 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :reserve_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-             numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { only_integer: true },
+             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
