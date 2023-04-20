6f2d99cad6bb716ea21fdf9e952f37ad7e2deeae
nininishishishi <121347235+nininishishishi@users.noreply.github.com>
2023-04-20 20:13:56 +0900

    Revert "Revert "レビュー依頼（商品削除機能）#1""
    This reverts commit 91e2a8d98776da9391cc7fb3df7e610f012b29ee.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが10,000,000以上の場合は保存できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
